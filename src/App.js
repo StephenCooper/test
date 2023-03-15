2ca49f7322ef424a0ff21c51e7bd6adebaea482d
Stephen Cooper <scooperdev@gmail.com>
2023-03-15 11:37:58 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { flushSync } from "react-dom";
+
+export default function PrintApp() {
+  const [isPrinting, setIsPrinting] = React.useState(false);
+
+  React.useEffect(() => {
+    function handleBeforePrint() {
+      flushSync(() => {
+        setIsPrinting(true);
+      });
+    }
+
+    function handleAfterPrint() {
+      setIsPrinting(false);
+    }
+
+    window.addEventListener("beforeprint", handleBeforePrint);
+    window.addEventListener("afterprint", handleAfterPrint);
+    return () => {
+      window.removeEventListener("beforeprint", handleBeforePrint);
+      window.removeEventListener("afterprint", handleAfterPrint);
+    };
+  }, []);
+
+  return (
+    <>
+      <h1>isPrinting: {isPrinting ? "yes" : "no"}</h1>
+      <button onClick={() => window.print()}>Print</button>
+    </>
+  );
+}
